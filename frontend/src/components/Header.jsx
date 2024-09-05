6fb757e87f019946b7d9c059c9819f36b087db1a
Mohammed Sinan <sinanksinu0@gmail.com>
2024-09-05 08:37:38 -0700

    contact page create

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -26,18 +26,26 @@ const Header = () => {
 
             <nav className={`absolute top-16 left-0 md:static w-full md:w-auto bg-slate-50 transition-all duration-300 ease-in-out ${navbarToggle ? 'block' : 'hidden'} md:flex`}>
                 <ul className='flex flex-col md:flex-row gap-1 sm:gap-5 text-xl font-semibold py-2 md:py-0'>
+                    <Link to='/'>
                     <li className='cursor-pointer px-8 md:px-0 py-2 md:py-0 hover:bg-slate-100 md:hover:bg-transparent md:hover:text-gray-500'>
                         Home
                     </li>
+                    </Link>
+                    <Link to='/collections'>
                     <li className='cursor-pointer px-8 md:px-0 py-2 md:py-0 hover:bg-slate-100 md:hover:bg-transparent md:hover:text-gray-500'>
                         Collections
                     </li>
+                    </Link>
+                    <Link to='/about'>
                     <li className='cursor-pointer px-8 md:px-0 py-2 md:py-0 hover:bg-slate-100 md:hover:bg-transparent md:hover:text-gray-500'>
                         About Us
                     </li>
+                    </Link>
+                    <Link to='/contact'>
                     <li className='cursor-pointer px-8 md:px-0 py-2 md:py-0 hover:bg-slate-100 md:hover:bg-transparent md:hover:text-gray-500'>
                         Contact Us
                     </li>
+                    </Link>
                 </ul>
             </nav>
 
