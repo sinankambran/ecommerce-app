121fb1e796b19ee422ccdd907488829eefe62498
Mohammed Sinan <sinanksinu0@gmail.com>
2024-09-14 22:44:22 -0700

    commit main

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -24,7 +24,7 @@ const RelatedProducts = ({ category, subCategory }) => {
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
                 {related.map((item, index) => (
                     <ProductItem 
-                        key={item._id} 
+                        key={index} 
                         id={item._id} 
                         name={item.name} 
                         price={item.price} 
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useCallback, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
@@ -7,11 +8,40 @@ const ShopContextProvider = (props) => {
     const currency = '$';
     const delivery_fee = '10';
     const [search, setSearch] = useState('');
-    const [showSearch,setShowSearch] = useState(true);
+    const [showSearch,setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId,size) => {
+        if (!size) {
+            toast.error('Select Product Size');
+            return; 
+        }
+         
+      let cartData = structuredClone(cartItems);
+      if (cartData[itemId]) {
+        if (cartData[itemId][size]){
+            cartData[itemId][size] += 1;
+        }
+        else{
+            cartData[itemId][size] = 1;
+        }
+      }
+      else{
+        cartData[itemId] = {};
+        cartData[itemId][size] = 1;
+      }
+      setCartItems(cartData);
+
+    }
+
+    useEffect(()=>{
+      console.log(cartItems);
+    },[cartItems])
 
     const value = {
         products, currency, delivery_fee,
-        search, setSearch, showSearch, setShowSearch
+        search, setSearch, showSearch, setShowSearch,
+        cartItems,addToCart
     }
 
     return (
